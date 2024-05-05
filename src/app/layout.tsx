9bfffb1037af8d0fa036804064d79259a4d7a594
arthuracrs <arthuracrs3@gmail.com>
2024-05-05 19:37:29 -0300

    save progress

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import React, { PropsWithChildren } from "react";
 import { PageLayout } from "@/components/page-layout";
 import { PreloadResources } from "@/app/preload-resources";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRoute'
+
 const RootLayout: React.FC<PropsWithChildren> = ({ children }) => {
   return (
     <html lang="en">
